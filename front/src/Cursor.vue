c710a3894b62c3cf13bdacc39f04cf6541e412dd
Forwindz <dfxxlyc@126.com>
2021-12-14 14:40:24 +0100

    Part of awareness design (code)

---
+++ front/src/Cursor.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <svg>
+            <g>
+                <polygon class="lineStyle" style="stroke:{{edgeColor}};fill:{{fillColor}}" points="7 6 12 18 14 13 19 11 7 6"/>
+            </g>
+        </svg>
+        <span class="mouseText">{{nick}}</span>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:"Cursor",
+    props:{
+        color:String,
+        nick:String
+    },
+    setup() {
+        
+    },
+    computed:{
+        edgeColor(){
+            return this.color;
+        },
+        fillColor(){
+            return this.color;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .svgStyle
+    .lineStyle{
+        stroke-linecap:round;
+        stroke-linejoin:round;
+    }
+    .mouseText{
+        font-size: 6px;
+    }
+</style>
+
+
